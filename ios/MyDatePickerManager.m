3794a8e67596e7b08fb47a4b270e42520b4d62df
uryyyyyyy <koki305@gmail.com>
2016-12-18 19:10:26 +0900

    tmp

--- ios/MyDatePickerManager.m
+++ ios/MyDatePickerManager.m
@@ -1,16 +1,32 @@
 #import "MyDatePickerManager.h"
 
+#import "RCTBridge.h"
+#import "MyDatePicker.h"
+
 @implementation MyDatePickerManager
 
 RCT_EXPORT_MODULE(MyDatePicker)
 
 - (UIView *)view {
-  return [[UIDatePicker alloc] init];
+  return [[MyDatePicker alloc] init];
 }
 
+- (UIDatePickerMode) myAction:(NSString *)modeStr {
+  if([modeStr isEqualToString:@"time"]){
+    return UIDatePickerModeTime;
+  }else if([modeStr isEqualToString:@"date"]){
+    return UIDatePickerModeDate;
+  }else { //datetime
+    return UIDatePickerModeDateAndTime;
+  }
+}
+
+
 RCT_EXPORT_VIEW_PROPERTY(date, NSDate)
+RCT_EXPORT_VIEW_PROPERTY(onMyChange, RCTBubblingEventBlock)
 RCT_CUSTOM_VIEW_PROPERTY(mode, UIDatePickerMode, UIDatePicker){
-  UIDatePickerMode pickerMode = UIDatePickerModeDateAndTime;
+  NSString *modeStr = json;
+  UIDatePickerMode pickerMode = [self myAction:modeStr];
   view.datePickerMode = pickerMode;
 }
 
