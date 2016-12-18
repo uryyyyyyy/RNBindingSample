c04df1b7931d7d5894bacb66ea9a054a205c59ca
uryyyyyyy <koki305@gmail.com>
2016-12-18 17:34:18 +0900

    swift

--- ios/MyDatePickerSwift.m
+++ ios/MyDatePickerSwift.m
@@ -2,6 +2,7 @@
 
 @interface RCT_EXTERN_MODULE(MyDatePickerManagerSwift, RCTViewManager)
 RCT_EXPORT_VIEW_PROPERTY(date, NSDate)
+RCT_EXPORT_VIEW_PROPERTY(onMyChange, RCTBubblingEventBlock)
 RCT_CUSTOM_VIEW_PROPERTY(mode, UIDatePickerMode, UIDatePicker){
   NSString *modeStr = json;
   UIDatePickerMode pickerMode = [self myAction:modeStr];
--- ios/MyDatePickerSwift.swift
+++ ios/MyDatePickerSwift.swift
@@ -4,9 +4,18 @@ import Foundation
 
 class MyDatePickerSwift : UIDatePicker {
   
+  var onMyChange:RCTBubblingEventBlock? = nil
+  
   override init(frame: CGRect) {
     super.init(frame: frame);
+    self.addTarget(self, action: #selector(self.buttonTapped(sender:)), for: UIControlEvents.valueChanged)
     self.frame = frame;
+  }
+  
+  func buttonTapped(sender: MyDatePickerSwift) {
+    let time = self.date.timeIntervalSince1970 * 1000.0
+    let dict: Dictionary = ["changedDate": time];
+    sender.onMyChange?(dict)
   }
   
   required init?(coder aDecoder: NSCoder) {
